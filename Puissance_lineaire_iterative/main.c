2ea599b89a10ea9677a08831fc95759b1d82cf98
Mirado14112003 <razafimandimbymirado@gmail.com>
2026-04-26 18:34:06 +0300

    Puissance linéaire itérative

---
+++ Puissance_lineaire_iterative/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long puissance_lineaire_iterative(long long a, unsigned int n){
+    if (n==0)
+        return 1;
+
+    long long resultat = 1;
+    for (int i=0; i<n; i++){
+        resultat *= a;
+        printf("Etape %u : resultat = %lld\n", i+1, resultat);
+    }
+    return resultat;
+}
+
+int main()
+{
+    long long a;
+    unsigned int n;
+    printf("Entrer le chiffre : ");
+    scanf("%lld", &a);
+    printf("Entrer l'exposant : ");
+    scanf("%u", &n);
+
+    long long resultat = puissance_lineaire_iterative(a, n);
+    printf("\n%lld^%u = %lld\n", a, n, resultat);
+    return 0;
+}
